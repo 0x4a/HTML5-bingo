96f2b0464d36bf6c21e1aae5eadd16e8965a2b57
hadez <github@follvalsch.de>
2016-01-19 11:31:37 +0100

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,7 +1,6 @@
+var headerText = "Mailinglisten Bingo";
 
-var headerText = "Bingo";
-
-var footerText = "<a href='https://github.com/jeffehobbs/HTML5-bingo/' target='_blank'>Code available on github</a>";
+var footerText = "<a href='https://github.com/hdznrrd/HTML5-bingo/' target='_blank'>This version's code available on github</a><br/><a href='https://github.com/jeffehobbs/HTML5-bingo/' target='_blank'>Original code available on github</a>";
 
 var winText = "Winner";
 
@@ -10,35 +9,45 @@ var clickSnd = new Audio("audio/click.mp3");
 var winSnd = new Audio("audio/win.mp3");
 
 var JSONBingo = {"squares": [
-        {"square": "one"},
-        {"square": "two"},
-        {"square": "three"},
-        {"square": "four"},
-        {"square": "five"},
-        {"square": "six"},
-        {"square": "seven"},
-        {"square": "eight"},
-        {"square": "nine"},
-        {"square": "ten"},
-        {"square": "eleven"},
-        {"square": "twelve"},
-        {"square": "thirteen"},
-        {"square": "fourteen"},
-        {"square": "fifteen"},
-        {"square": "sixteen"},
-        {"square": "seventeen"},
-        {"square": "eighteen"},
-        {"square": "nineteen"},
-        {"square": "twenty"},
-        {"square": "twenty one"},
-        {"square": "twenty two"},
-        {"square": "twenty three"},
-        {"square": "twenty four"},    	
-        {"square": "twenty five"},    	
-        {"square": "twenty six"},    	
-        {"square": "twenty seven"}, 
-        {"square": "twenty eight"},    	
-        {"square": "twenty nine"},    	
-        {"square": "thirty"}    	
+        {"square": "Mehr Transparenz!"},
+        {"square": "Plenum soll entscheiden"},
+        {"square": "Vorstand soll entscheiden"},
+        {"square": "Da gibt es einen Beschluss"},
+        {"square": "Ad hominem"},
+        {"square": "Moving the goalposts"},
+        {"square": "Anekdotische Evidenz"},
+        {"square": "Berufung auf Wahrscheinlich-keit"},
+        {"square": "Strohmann"},
+        {"square": "Argumentum ad ignorantiam"},
+        {"square": "Ad nauseam"},
+        {"square": "Petitio principii"},
+        {"square": "Beweislast-umkehr"},
+        {"square": "Zirkelschluss"},
+        {"square": "Korrelation und Kausation verwechselt"},
+        {"square": "Argumentum ad verecundiam"},
+        {"square": "Falsches Dilemma"},
+        {"square": "Fangfrage"},
+        {"square": "Spieler-fehlschluss"},
+        {"square": "Unvollst&auml;ndiger Vergleich"},
+        {"square": "(Fehl)-interpretation der Intention"},
+        {"square": "Ignoratio elenchi"},
+        {"square": "Moral high groun"},
+        {"square": "Natura-listischer Fehlschluss"},
+        {"square": "Post hoc ergo propter hoc"},
+        {"square": "Beweis durch Wiederholung"},
+        {"square": "Argumentum verbosium"},
+        {"square": "Berufung auf Emotionen"},
+        {"square": "Argumentum ad novitatem"},
+        {"square": "Argumentum ad antiquitatem"},
+        {"square": "Argumentum ad baculum"},
+        {"square": "Argumentum ad populum"},
+        {"square": "First world problem"},
+        {"square": "Mimimi auf hohem Niveau"},
+        {"square": "Pis erstes Gesetz"},
+        {"square": "Pis zweites Gesetz"},
+        {"square": "Godwin"},
+        {"square": "Doppelt falsch ist gut"},
+        {"square": "Broken window"},
+        {"square": "Slippery slope"},
     ]
 };
