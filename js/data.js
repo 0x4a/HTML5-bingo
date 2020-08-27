5469efca79200d6d49d5cc82ae9a919595eefefb
Daniel Jackel <dev@0x4a.net>
2020-08-27 19:07:55 -0400

    Support-Bingo first draft

--- js/data.js
+++ js/data.js
@@ -1,70 +1,38 @@
-var headerText = "Mailinglisten Bingo";
+var headerText = "Support-Bingo";
 
-var footerText = "<a href='https://github.com/hdznrrd/HTML5-bingo/' target='_blank'>This version's code available on github</a><br/><a href='https://github.com/jeffehobbs/HTML5-bingo/' target='_blank'>Original code available on github</a><br/><a href='http://www.bruhaha.de/laws.html' target='_blank'>Manche Begriffe stammen aus den Usenet-Laws</a><br/><a href='https://en.wikipedia.org/wiki/List_of_logical_fallacies' target='_blank'>Wikipedia: List of logical fallacies</a>";
+var footerText = "Original code <a href='https://github.com/jeffehobbs/HTML5-bingo/' target='_blank'>available on github</a>";
 
-var winText = "The Game.";
+var winText = "BINGO!";
 
 var clickSnd = new Audio("audio/click.mp3");
 
 var winSnd = new Audio("audio/win.mp3");
 
 var JSONBingo = {"squares": [
-        {"square": "Mehr Transparenz!"},
-        {"square": "Plenum soll entscheiden"},
-        {"square": "Vorstand soll entscheiden"},
-        {"square": "Da gibt es einen Beschluss"},
-        {"square": "Ad hominem"},
-        {"square": "Ad personam"},
-        {"square": "Moving the goalposts"},
-        {"square": "Anekdotische Evidenz"},
-        {"square": "Berufung auf Wahrscheinlich-keit"},
-        {"square": "Strohmann"},
-        {"square": "Argumentum ad ignorantiam"},
-        {"square": "Ad nauseam"},
-        {"square": "Petitio principii"},
-        {"square": "Beweislast-umkehr"},
-        {"square": "Zirkelschluss"},
-        {"square": "Korrelation und Kausation verwechselt"},
-        {"square": "Argumentum ad verecundiam"},
-        {"square": "Falsches Dilemma"},
-        {"square": "Fangfrage"},
-        {"square": "Spieler-fehlschluss"},
-        {"square": "Unvollst&auml;ndiger Vergleich"},
-        {"square": "(Fehl)-interpretation der Intention"},
-        {"square": "Ignoratio elenchi"},
-        {"square": "Moral high ground"},
-        {"square": "Natura-listischer Fehlschluss"},
-        {"square": "Post hoc, ergo propter hoc"}, 
-        {"square": "Cum hoc, ergo propter hoc"},
-        {"square": "Beweis durch Wiederholung"},
-        {"square": "Argumentum verbosium"},
-        {"square": "Berufung auf Emotionen"},
-        {"square": "Argumentum ad novitatem"},
-        {"square": "Argumentum ad antiquitatem"},
-        {"square": "Argumentum ad baculum"},
-        {"square": "Argumentum ad populum"},
-        {"square": "First world problem"},
-        {"square": "Mimimi auf hohem Niveau"},
-        {"square": "Pis erstes Gesetz"},
-        {"square": "Pis zweites Gesetz"},
-        {"square": "Godwin"},
-        {"square": "Doppelt falsch ist gut"},
-        {"square": "Broken window"},
-        {"square": "Slippery slope"},
-        {"square": "Blockwart schreitet ein"},
-        {"square": "Argumentum ad mailclient"},
-        {"square": "Wer hat Schuld"},
-        {"square": "Wer ist denn Verantwortlich"},
-        {"square": "Rehctschreibung"},  // absichtsvoller Schreibfehler
-        {"square": "So hab ich das nicht gesagt"},
-        {"square": "Fr&uuml;her haben wir das auch so gemacht"},
-        {"square": "Confirmation Bias"},
-        {"square": "tu quoque"},
-        {"square": "Mereologischer Fehlschluss"},
-        {"square": "Argument ad temperantiam"}, // übersetzt als: siehe nächste Zeile
-        {"square": "Schiedrichter-Fehlschluss"},
-        {"square": "Guilt by Association"},
-        {"square": "Argument ex silentio"},  // http://www.hoheluft-magazin.de/2016/08/na-logisch-das-argument-ex-silentio/
-        {"square": "Psychologistischer Fehlschluss"} // http://www.hoheluft-magazin.de/2016/10/na-logisch-der-psychologistische-fehlschluss/
+        {"square": "one"},
+        {"square": "two"},
+        {"square": "three"},
+        {"square": "four"},
+        {"square": "five"},
+        {"square": "six"},
+        {"square": "seven"},
+        {"square": "eight"},
+        {"square": "nine"},
+        {"square": "ten"},
+        {"square": "eleven"},
+        {"square": "twelve"},
+        {"square": "thirteen"},
+        {"square": "fourteen"},
+        {"square": "fifteen"},
+        {"square": "sixteen"},
+        {"square": "seventeen"},
+        {"square": "eighteen"},
+        {"square": "nineteen"},
+        {"square": "twenty"},
+        {"square": "twenty-one"},
+        {"square": "twenty-two"},
+        {"square": "twenty-three"},
+        {"square": "twenty-four"},
+        {"square": "twenty-five"}
     ]
 };
